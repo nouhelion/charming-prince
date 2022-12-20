209372c380ca53754db1aaa087d3d793cdc92a16
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2022-12-20 13:37:06 +0100

    dying

---
+++ clothing/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 51, 153, 255);
+const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+const double defaultPadding = 16.0;
